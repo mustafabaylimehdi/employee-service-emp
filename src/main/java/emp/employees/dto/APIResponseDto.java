7decbee615d69981f4afe8e89ffb5074ddd292d0
mustafabeylimm <mustafabayli.mehdi07@gmail.com>
2023-04-03 10:40:55 +0400

    Feign Client added

---
+++ src/main/java/emp/employees/dto/APIResponseDto.java
@@ -0,0 +1,15 @@
+package emp.employees.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class APIResponseDto {
+    private DepartmentDto department;
+    private EmployeeDto employee;
+}
